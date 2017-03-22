a79a1c2f1ac5265dd52cc6583413a96345f7b282
a28115665 <a28115665@gmail.com>
2017-03-22 17:51:15 +0800

    20170322 add Restful page to test

--- app/Restful/module.js
+++ app/Restful/module.js
@@ -20,68 +20,135 @@ angular.module('app.restful').config(function ($stateProvider){
 			views: {
 				"content@app" : {
 					templateUrl: 'app/Restful/views/test.html',
-                    controller: function ($scope, config, RestfulApi) {
+                    controller: function ($scope, config, RestfulApi, Session, $filter) {
                         
-                    	// var self = {};
-                    	this.data = config["returnData"];
-                    	console.log(config);
+                        var $vm = this;
+
+                        angular.extend(this, {
+                            // data : config["returnData"],
+                            testData : {
+                                ID : "",
+                                Name : ""
+                            },
+                            restful : {
+                                queryTest : {
+                                    status : "",
+                                    result : ""
+                                },
+                                insertTest : {
+                                    status : "",
+                                    result : ""
+                                },
+                                updateTest : {
+                                    status : "",
+                                    result : ""
+                                },
+                                deleteTest : {
+                                    status : "",
+                                    result : ""
+                                }
+                            }
+                        });
+
+                        /**
+                         * Query Sample
+                         */
+                        $vm.QueryTest = function(){
+                            RestfulApi.SearchMSSQLData({
+                                queryname: 'SelectAllUserInfo',
+                                params: {
+                                    U_ID : $vm.testData.ID
+                                }
+                            }).then(function (res){
+                                $vm.restful.queryTest.status = "成功";
+                                $vm.restful.queryTest.result = {
+                                    ID : res["returnData"][0]["U_ID"],
+                                    Name : res["returnData"][0]["U_Name"]
+                                };
+                            }, function (err){
+                                $vm.restful.queryTest.status = "失敗";
+                                $vm.restful.queryTest.result = err;
+                            });
+                        };
 
                         /**
                          * Insert Sample
                          */
-                        // RestfulApi.InsertMSSQLData({
-                        //     insertname: 'Insert',
-                        //     table: 0,
-                        //     params: {
-                        //         U_ID : "ttt",
-                        //         U_Name : "aaa"
-                        //     }
-                        // }).then(function(res) {
-                        //     console.log(res);
-                        // });
+                        $vm.InsertTest = function(){
+                            RestfulApi.InsertMSSQLData({
+                                insertname: 'Insert',
+                                table: 0,
+                                params: {
+                                    U_ID : $vm.testData.ID,
+                                    U_Name : $vm.testData.Name,
+                                    U_PW : "TEST",
+                                    U_Check : true,
+                                    U_CR_User : Session.Get().U_ID,
+                                    U_CR_DateTime : $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss')
+                                }
+                            }).then(function (res) {
+                                $vm.restful.insertTest.status = "成功";
+                                $vm.restful.insertTest.result = res["returnData"];
+                            }, function (err) {
+                                $vm.restful.insertTest.status = "失敗";
+                                $vm.restful.insertTest.result = err;
+                            });
+                        };
 
                         /**
                          * Update Sample
                          */
-                        // RestfulApi.UpdateMSSQLData({
-                        //     updatename: 'Update',
-                        //     table: 0,
-                        //     params: {
-                        //         U_Name : "gggg"
-                        //     },
-                        //     condition: {
-                        //         U_ID : "ttt"
-                        //     }
-                        // }).then(function(res) {
-                        //     console.log(res);
-                        // });
+                        $vm.UpdateTest = function(){
+                            RestfulApi.UpdateMSSQLData({
+                                updatename: 'Update',
+                                table: 0,
+                                params: {
+                                    U_Name : $vm.testData.Name,
+                                    U_UP_User : Session.Get().U_ID,
+                                    U_UP_DateTime : $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss')
+                                },
+                                condition: {
+                                    U_ID : $vm.testData.ID
+                                }
+                            }).then(function (res) {
+                                $vm.restful.updateTest.status = "成功";
+                                $vm.restful.updateTest.result = res["returnData"];
+                            }, function (err) {
+                                $vm.restful.updateTest.status = "失敗";
+                                $vm.restful.updateTest.result = err;
+                            });
+                        };
 
                         /**
                          * Delete Sample
                          */
-                        // RestfulApi.DeleteMSSQLData({
-                        //     deletename: 'Delete',
-                        //     table: 0,
-                        //     params: {
-                        //         U_ID : "ttt"
-                        //     }
-                        // }).then(function(res) {
-                        //     console.log(res);
-                        // });
+                        $vm.DeleteTest = function(){
+                            RestfulApi.DeleteMSSQLData({
+                                deletename: 'Delete',
+                                table: 0,
+                                params: {
+                                    U_ID : $vm.testData.ID
+                                }
+                            }).then(function (res) {
+                                $vm.restful.deleteTest.status = "成功";
+                                $vm.restful.deleteTest.result = res["returnData"];
+                            }, function (err) {
+                                $vm.restful.deleteTest.status = "失敗";
+                                $vm.restful.deleteTest.result = err;
+                            });
+                        };
+
                     },
-                    controllerAs: 'alanTestCtrl',
+                    controllerAs: '$vm',
                     resolve: {
                         config: function (RestfulApi) {
-                        	/**
-                        	 * Select Sample
-                        	 */
-                            return RestfulApi.SearchMSSQLData({
-                                queryname: 'SelectAllUserInfo',
-		                        params: {
-		                            U_ID : "Admin",
-		                            U_Name : "系統管理員"
-		                        }
-                            });
+                          //   return RestfulApi.SearchMSSQLData({
+                          //       queryname: 'SelectAllUserInfo',
+		                        // params: {
+		                        //     U_ID : "Admin",
+		                        //     U_Name : "系統管理員"
+		                        // }
+                          //   });
                         }
                     }
 				}
