e633de21db6ef57e408e1119cc97efb048ba62bf
a28115665 <a28115665@gmail.com>
2017-04-19 15:54:28 +0800

    20170419 menu經由json產生的方式

--- app/_common/layout/directives/navigation/smartMenuItems.js
+++ app/_common/layout/directives/navigation/smartMenuItems.js
@@ -4,65 +4,8 @@
     angular.module('SmartAdmin.Layout').directive('smartMenuItems', function ($http, $rootScope, $compile) {
     return {
         restrict: 'A',
-        compile: function (element, attrs) {
+        // compile: function (element, attrs) {
             
-            function createItem(item, parent, level){
-                var li = $('<li />' ,{'ui-sref-active': "active"})
-                var a = $('<a />');
-                var i = $('<i />');
-
-                li.append(a);
-
-                if(item.sref)
-                    a.attr('ui-sref', item.sref);
-                if(item.href)
-                    a.attr('href', item.href);
-                if(item.icon){
-                    i.attr('class', 'fa fa-lg fa-fw fa-'+item.icon);
-                    a.append(i);
-                }
-                if(item.title){
-                    a.attr('title', item.title);
-                    if(level == 1){ 
-                        console.log(item.title, $rootScope.getWord(item.title));
-                        a.append(' <span class="menu-item-parent">' + item.title + '</span>');
-                    } else {
-                        a.append(' ' + item.title);
-
-                    }
-                }
-
-                if(item.items){
-                    var ul = $('<ul />');
-                    li.append(ul);
-                    li.attr('data-menu-collapse', '');
-                    _.forEach(item.items, function(child) {
-                        createItem(child, ul, level+1);
-                    })
-                } 
-
-                parent.append(li); 
-            }
-
-            $http.get(attrs.smartMenuItems).then(function(res){
-                var ul = $('<ul />', {
-                    'smart-menu': ''
-                })
-                _.forEach(res.data.items, function(item) {
-                    createItem(item, ul, 1);
-                })
-                
-                var $scope = $rootScope.$new();
-                var html = $('<div>').append(ul).html(); 
-                var linkingFunction = $compile(html);
-                
-                var _element = linkingFunction($scope);
-
-                element.replaceWith(_element);
-                console.log(element);                
-            })
-        },
-        // link: function(scope, element, attrs){
         //     function createItem(item, parent, level){
         //         var li = $('<li />' ,{'ui-sref-active': "active"})
         //         var a = $('<a />');
@@ -82,8 +25,7 @@
         //             a.attr('title', item.title);
         //             if(level == 1){ 
         //                 console.log(item.title, $rootScope.getWord(item.title));
-        //                 // a.append(' <span class="menu-item-parent">' + item.title + '</span>');
-        //                 a.append(' <span class="menu-item-parent">' + $rootScope.getWord(item.title) + '</span>');
+        //                 a.append(' <span class="menu-item-parent">' + item.title + '</span>');
         //             } else {
         //                 a.append(' ' + item.title);
 
@@ -102,33 +44,91 @@
         //         parent.append(li); 
         //     }
 
-
-        //     function DoMenu(){
-        //         $http.get(attrs.smartMenuItems).then(function(res){
-        //             var ul = $('<ul />', {
-        //                 'smart-menu': ''
-        //             })
-        //             _.forEach(res.data.items, function(item) {
-        //                 createItem(item, ul, 1);
-        //             })
-                    
-        //             var $scope = $rootScope.$new();
-        //             var html = $('<div>').append(ul).html(); 
-        //             var linkingFunction = $compile(html);
-                    
-        //             var _element = linkingFunction($scope);
-
-        //             element.replaceWith(_element);
-        //             console.log(element);                
+        //     $http.get(attrs.smartMenuItems).then(function(res){
+        //         var ul = $('<ul />', {
+        //             'smart-menu': ''
         //         })
-        //     }
+        //         _.forEach(res.data.items, function(item) {
+        //             createItem(item, ul, 1);
+        //         })
+                
+        //         var $scope = $rootScope.$new();
+        //         var html = $('<div>').append(ul).html(); 
+        //         var linkingFunction = $compile(html);
+                
+        //         var _element = linkingFunction($scope);
 
-        //     $rootScope.$watch('lang', function(newVal, oldVal){
-        //         if(!angular.equals(newVal, {}) && !angular.isUndefined(newVal)){
-        //             DoMenu();
-        //         }
-        //     }, true);
-        // }
+        //         element.replaceWith(_element);
+        //         console.log(element);                
+        //     })
+        // },
+        link: function(scope, element, attrs){
+            function createItem(item, parent, level){
+                var li = $('<li />' ,{'ui-sref-active': "active"})
+                var a = $('<a />');
+                var i = $('<i />');
+
+                li.append(a);
+
+                if(item.sref)
+                    a.attr('ui-sref', item.sref);
+                if(item.href)
+                    a.attr('href', item.href);
+                if(item.icon){
+                    i.attr('class', 'fa fa-lg fa-fw fa-'+item.icon);
+                    a.append(i);
+                }
+                if(item.title){
+                    a.attr('title', $rootScope.getWord(item.title));
+                    if(level == 1){ 
+                        a.append(' <span class="menu-item-parent">' + $rootScope.getWord(item.title) + '</span>');
+                    } else {
+                        a.append(' ' + $rootScope.getWord(item.title));
+
+                    }
+                }
+
+                if(item.items){
+                    var ul = $('<ul />');
+                    li.append(ul);
+                    li.attr('data-menu-collapse', '');
+                    _.forEach(item.items, function(child) {
+                        createItem(child, ul, level+1);
+                    })
+                } 
+
+                parent.append(li); 
+            }
+
+
+            function DoMenu(){
+                $http.get(attrs.smartMenuItems).then(function(res){
+                    console.log(res);
+                    var ul = $('<ul />', {
+                        'smart-menu': ''
+                    })
+                    _.forEach(res.data.items, function(item) {
+                        createItem(item, ul, 1);
+                    })
+                    
+                    var $scope = $rootScope.$new();
+                    var html = $('<div>').append(ul).html(); 
+                    var linkingFunction = $compile(html);
+                    
+                    var _element = linkingFunction($scope);
+                    // console.log(_element);
+                    // element.replaceWith(_element);
+                    
+                    element.html(_element);           
+                })
+            }
+
+            $rootScope.$watch('lang', function(newVal, oldVal){
+                if(!angular.equals(newVal, {}) && !angular.isUndefined(newVal)){
+                    DoMenu();
+                }
+            }, true);
+        }
     }
 });
 })();
