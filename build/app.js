574572100a6baea029262e8a6f46f12e465749b7
a28115665 <a28115665@gmail.com>
2017-04-23 22:00:11 +0800

    20170423 報機單併單結果初版

--- build/app.js
+++ build/app.js
@@ -11711,6 +11711,29 @@ angular.module('app.selfwork').controller('Job001Ctrl', function ($scope, $state
         },
         Update : function(){
 
+        },
+        MergeNoResult : function(){
+            var modalInstance = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'mergeNoResultModalContent.html',
+                controller: 'MergeNoResultModalInstanceCtrl',
+                controllerAs: '$ctrl',
+                size: 'lg',
+                // appendTo: parentElem,
+                resolve: {
+                    job001Data: function() {
+                        return $vm.job001Data;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(selectedItem) {
+                // $ctrl.selected = selectedItem;
+            }, function() {
+                // $log.info('Modal dismissed at: ' + new Date());
+            });
         }
     });
 
@@ -11744,6 +11767,123 @@ angular.module('app.selfwork').controller('Job001Ctrl', function ($scope, $state
     $ctrl.selected = {
         item: $ctrl.items[0]
     };
+
+    $ctrl.ok = function() {
+        $uibModalInstance.close($ctrl.selected.item);
+    };
+
+    $ctrl.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+})
+.controller('MergeNoResultModalInstanceCtrl', function ($uibModalInstance, job001Data, uiGridGroupingConstants) {
+    var $ctrl = this;
+    $ctrl.items = job001Data;
+
+    $ctrl.MdInit = function(){
+        DoMergeNoSplit();
+
+        // console.log($ctrl.job001DataHaveMergeNo);
+        // console.log($ctrl.job001DataNotMergeNo);
+    };
+
+    $ctrl.HandleWindowResize = function(pGridApi){
+        HandleWindowResize(pGridApi);
+    }
+
+    $ctrl.job001DataHaveMergeNoOption = {
+        data: '$ctrl.job001DataHaveMergeNo',
+        columnDefs: [
+            { name: 'Index',        displayName: '序列', width: 50},
+            { name: 'IL_G1',        displayName: '報關種類', width: 154 },
+            { name: 'IL_BAGNO',        displayName: '袋號', width: 129 },
+            { name: 'IL_MERGENO',        displayName: '併票號', width: 129, grouping: { groupPriority: 0 } },
+            { name: 'IL_SMALLNO',        displayName: '小號', width: 115 },
+            { name: 'IL_NATURE',        displayName: '品名', width: 115 },
+            { name: 'IL_NATURE_NEW',        displayName: '新品名', width: 115 },
+            { name: 'IL_CTN',        displayName: '件數', width: 115, treeAggregationType: uiGridGroupingConstants.aggregation.SUM, customTreeAggregationFinalizerFn: function( aggregation ) {
+                    aggregation.rendered = aggregation.value;
+                }
+            },
+            { name: 'IL_PLACE',        displayName: '產地', width: 115 },
+            { name: 'IL_WEIGHT',        displayName: '重量', width: 115, treeAggregationType: uiGridGroupingConstants.aggregation.SUM, customTreeAggregationFinalizerFn: function( aggregation ) {
+                    // console.log(aggregation);
+                    aggregation.rendered = aggregation.value;
+                }
+            },
+            { name: 'IL_WEIGHT_NEW',        displayName: '更改後重量', width: 115 },
+            { name: 'IL_PCS',        displayName: '數量', width: 115 },
+            { name: 'IL_UNIT',        displayName: '單位', width: 115 },
+            { name: 'IL_GETNO',        displayName: '收件者統編', width: 115 },
+            { name: 'IL_SENDNAME',        displayName: '寄件人或公司', width: 115 },
+            { name: 'IL_GETNAME',        displayName: '收件人公司', width: 115 },
+            { name: 'IL_GETADDRESS',        displayName: '收件地址', width: 300 },
+            { name: 'IL_GETTEL',        displayName: '收件電話', width: 115 },
+            { name: 'IL_UNIVALENT',        displayName: '單價', width: 115 },
+            { name: 'IL_FINALCOST',        displayName: '完稅價格', width: 115 },
+            { name: 'IL_TAX',        displayName: '稅則', width: 115 },
+            { name: 'IL_TRCOM',        displayName: '派送公司', width: 115 }
+        ],
+        enableFiltering: false,
+        enableSorting: true,
+        enableColumnMenus: false,
+        // enableVerticalScrollbar: false,
+        paginationPageSizes: [50, 100, 150, 200, 250, 300],
+        paginationPageSize: 50,
+        onRegisterApi: function(gridApi){
+            $ctrl.job001DataHaveMergeNoGridApi = gridApi;
+        }
+    };
+
+    $ctrl.job001DataNotMergeNoOption = {
+        data: '$ctrl.job001DataNotMergeNo',
+        columnDefs: [
+            { name: 'Index',        displayName: '序列', width: 50},
+            { name: 'IL_G1',        displayName: '報關種類', width: 154 },
+            { name: 'IL_BAGNO',        displayName: '袋號', width: 129 },
+            { name: 'IL_MERGENO',        displayName: '併票號', width: 129 },
+            { name: 'IL_SMALLNO',        displayName: '小號', width: 115 },
+            { name: 'IL_NATURE',        displayName: '品名', width: 115 },
+            { name: 'IL_NATURE_NEW',        displayName: '新品名', width: 115 },
+            { name: 'IL_CTN',        displayName: '件數', width: 115 },
+            { name: 'IL_PLACE',        displayName: '產地', width: 115 },
+            { name: 'IL_WEIGHT',        displayName: '重量', width: 115 },
+            { name: 'IL_WEIGHT_NEW',        displayName: '更改後重量', width: 115 },
+            { name: 'IL_PCS',        displayName: '數量', width: 115 },
+            { name: 'IL_UNIT',        displayName: '單位', width: 115 },
+            { name: 'IL_GETNO',        displayName: '收件者統編', width: 115 },
+            { name: 'IL_SENDNAME',        displayName: '寄件人或公司', width: 115 },
+            { name: 'IL_GETNAME',        displayName: '收件人公司', width: 115 },
+            { name: 'IL_GETADDRESS',        displayName: '收件地址', width: 300 },
+            { name: 'IL_GETTEL',        displayName: '收件電話', width: 115 },
+            { name: 'IL_UNIVALENT',        displayName: '單價', width: 115 },
+            { name: 'IL_FINALCOST',        displayName: '完稅價格', width: 115 },
+            { name: 'IL_TAX',        displayName: '稅則', width: 115 },
+            { name: 'IL_TRCOM',        displayName: '派送公司', width: 115 }
+        ],
+        enableFiltering: false,
+        enableSorting: true,
+        enableColumnMenus: false,
+        // enableVerticalScrollbar: false,
+        paginationPageSizes: [50, 100, 150, 200, 250, 300],
+        paginationPageSize: 50,
+        onRegisterApi: function(gridApi){
+            $ctrl.job001DataNotMergeNoGridApi = gridApi;
+        }
+    }
+
+    function DoMergeNoSplit(){
+        $ctrl.job001DataHaveMergeNo = [];
+        $ctrl.job001DataNotMergeNo = [];
+
+        for(var i in job001Data){
+            if(job001Data[i].IL_MERGENO){
+                $ctrl.job001DataHaveMergeNo.push(job001Data[i]);
+            }else{
+                $ctrl.job001DataNotMergeNo.push(job001Data[i]);
+            }
+        }
+    }
 
     $ctrl.ok = function() {
         $uibModalInstance.close($ctrl.selected.item);
